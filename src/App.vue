56f864fefcb16d00b2323b675bd5cd3168dc5570
Thav Sievny <kneythav08@gmail.com>
2020-06-09 16:40:03 +0700

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,167 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input
+         type="text"
+         class="search-bar"
+         placeholder="Search..."
+         v-model="query"
+         @keypress="fetchWeather"
+         />
+
+      </div><br>
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">TUESDAY 9 JUNE 2020</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}°C</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      api_key: '656665aeeb8b6169ccb6bf35e7228997',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
+  methods: {
+    fetchWeather (e) {
+      if (e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    },
+    dateBuilder () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  }
+body {
+  font-family: 'Roboto Mono', monospace;
+
+}
+#app {
+  background-image: url('./assets/cold.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+  main {
+    min-height: 100vh;
+    padding: 25px;
+
+    background-image: linear-gardient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75))
+  };
+  .search-box {
+    width: 100%;
+    margin-bottom: 30px;
+  }
+  .search-box .search-bar {
+    display: block;
+    width: 100%;
+    padding: 15px;
+    color: #313131;
+    font-size: 20px;
+
+    appearance: none;
+    border: none;
+    outline: none;
+    background: none;
+
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.5);
+    border-radius: 0px 16px 0px 16px; 
+    transition: 0.4s;
+  }
+  .search-box .search-bar:focus {
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+    background-color: #f0f0f0;
+    border-radius: 16px 0px 16px 0px;
+
+  }
+  .location-box .location {
+    color: #fffdf9;
+    font-size: 32px;
+    font-weight: 500;
+    font-style: Lexend Tera;
+    text-align: left;
+    text-shadow: 1px 3px rgba (0, 0, 0, 0.25);
+
+    background-color: #30475e;
+    border-radius: 16px;
+    margin: 30px 0px;
+  }
+  .location-box .date {
+    color: #fffdf9;
+    font-size: 32px;
+    font-weight: 300;
+    font-style: italic;
+    text-align: left;
+
+    background-color: #30475e;
+    border-radius: 16px;
+    margin: 30px 0px;
+  }
+  .weather-box {
+    text-align: left;
+  }
+  .weather-box .temp {
+    display: inline-block;
+    padding: 10px 25px;
+    color: #fffdf9;
+    font-size: 102px;
+    font-weight: 900px;
+
+    background-color: #30475e;
+    border-radius: 16px;
+    margin: 30px 0px;
+
+    box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  }
+  .weather-box .weather {
+    color: #fffdf9;
+    font-size: 48px;
+    font-weight: 700px;
+    font-style: italic;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+
+    background-color: #30475e;
+    border-radius: 16px;
+    margin: 30px 0px;
+  }
+</style>
